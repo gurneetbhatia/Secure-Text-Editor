5974998218ea4e98481f092885457069ae9af495
bgurneet <sbgurneet@gmail.com>
2020-04-25 12:29:44 +0400

    Added passwd protection for keys

--- filesystem.py
+++ filesystem.py
@@ -19,33 +19,35 @@ class FileSystem:
         finally:
             return isNew
 
-    def createOrganisation(self, organisation):
+    def createOrganisation(self, organisation, password):
         keyPath = 'localkeys/'+organisation+'.key'
         enc = Encrypt()
+        password = bytes(password, encoding='utf-8')
         # enc.privateKey needs to be saved
         pem = enc.privateKey.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
+        encryption_algorithm=serialization.BestAvailableEncryption(password)
         )
         with open(keyPath, 'wb') as f:
             f.write(pem)
 
-    def getOrganisationKey(self, organisation):
+    def getOrganisationKey(self, organisation, password):
         keyPath = 'localkeys/'+organisation+'.key'
         privateKey = None
+        password = bytes(password, encoding='utf-8')
         with open(keyPath, 'rb') as key_file:
             privateKey = serialization.load_pem_private_key(
             key_file.read(),
-            password=None,
+            password=password,
             backend=default_backend()
             )
         return privateKey
 
-    def importFile(self, filepath, organisation, savepath=None):
+    def importFile(self, filepath, organisation, password, savepath=None):
         # first check if the organisation is is a new one
         if self.isNewOrganisation(organisation):
-            self.createOrganisation(organisation)
+            self.createOrganisation(organisation, password)
 
         # read the file they provided, encrypt the contents and save it
         original_file = open(filepath, 'r')
@@ -53,7 +55,7 @@ class FileSystem:
         original_file.close()
 
         # load the key for the organisation
-        key = self.getOrganisationKey(organisation)
+        key = self.getOrganisationKey(organisation, password)
         enc = Encrypt(key)
         encrypted_contents = enc.encrypt_string(original_contents)
         savepath = savepath+'.enc' if savepath == None else savepath
@@ -61,9 +63,9 @@ class FileSystem:
         encrypted_file.write(encrypted_contents)
         encrypted_file.close()
 
-    def readFile(self, filepath, organisation):
+    def readFile(self, filepath, organisation, password):
         # get the key for the provided organisation
-        key = self.getOrganisationKey(organisation)
+        key = self.getOrganisationKey(organisation, password)
         enc = Encrypt(key)
 
         encrypted_file = open(filepath, 'rb')
@@ -73,10 +75,10 @@ class FileSystem:
         decrypted_contents = enc.decrypt_string(encrypted_contents)
         return decrypted_contents
 
-    def updateFile(self, filepath, contents, organisation):
+    def updateFile(self, filepath, contents, organisation, password):
         # this function can be used when the user chooses to save their work
         # get the key for the provided organisation
-        key = self.getOrganisationKey(organisation)
+        key = self.getOrganisationKey(organisation, password)
         enc = Encrypt(key)
 
         encrypted_file = open(filepath, 'wb')
@@ -87,7 +89,8 @@ class FileSystem:
 
 if __name__ == '__main__':
     f = FileSystem()
-    f.importFile('test.py', 'Student Hack', 'test.py.enc')
-    print(f.readFile('test.py.enc', 'Student Hack'))
-    f.updateFile('test.py.enc', 'print("hello world 1")', 'Student Hack')
-    print(f.readFile('test.py.enc', 'Student Hack'))
+    f.importFile('test.py', 'Student Hack', 'test1234', 'test.py.enc')
+    print(f.readFile('test.py.enc', 'Student Hack', 'test1234'))
+    f.updateFile('test.py.enc', 'print("hello world 1")', 'Student Hack',
+    'test1234')
+    print(f.readFile('test.py.enc', 'Student Hack', 'test1234'))

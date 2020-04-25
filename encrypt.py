4fc0a16ff912b89f7bbdc7deae33142718ae1e17
bgurneet <sbgurneet@gmail.com>
2020-04-25 10:44:08 +0400

    started working on encryption module

---
+++ encrypt.py
@@ -0,0 +1,35 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+#from cryptography.hazmat.primitives import serialization
+
+class Encrypt:
+    def __init__(self):
+        self.privateKey = self.getNewKey()
+
+    def getNewKey(self):
+        return rsa.generate_private_key(public_exponent=65537,
+        key_size=2048,
+        backend=default_backend())
+
+    def getPublicKey(self):
+        return self.privateKey.public_key()
+
+    def encrypt_string(self, string):
+        string = bytes(string, encoding='utf-8')
+        return self.getPublicKey().encrypt(string,
+        padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+        ))
+
+    def decrypt_string(self, string):
+        msg = self.privateKey.decrypt(string,
+        padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+        ))
+        return msg.decode('utf-8')

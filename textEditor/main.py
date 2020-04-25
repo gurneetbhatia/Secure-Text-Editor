25082a9e3bc1333d0b64f2118878165ff2e4e886
Finn Hudson <11finnh@gmail.com>
2020-04-25 17:58:51 +0100

    Layout added

--- textEditor/main.py
+++ textEditor/main.py
@@ -5,28 +5,29 @@ from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
 from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
 
 class NavBarController(Widget):
-    def setup(self, width, height, btns, bk):
+    def setup(self, layout):
+        org_btn = Button(text='File')
+        import_btn = Button(text='Organisation')
+        save_btn = Button(text='Run')
+        run_btn = Button(text='Help')
+
+        btns = [org_btn, import_btn, save_btn, run_btn]
+
+        layout.add_widget(btns[0])
+        layout.add_widget(btns[1])
+        layout.add_widget(btns[2])
+        layout.add_widget(btns[3])
+
         btns[0].bind(on_press=NavBarController.org_btn_press)
         btns[1].bind(on_press=NavBarController.import_btn_press)
         btns[2].bind(on_press=NavBarController.save_btn_press)
         btns[3].bind(on_press=NavBarController.run_btn_press)
 
         numButtons = len(btns)
-        btnWidth = width/numButtons
-        btnHeight = height/20
-
-        for index, btn in enumerate(btns):
-            btn.width = btnWidth
-            btn.height = btnHeight
-            btn.y = height - btnHeight
-            btn.x = 0 + index*btnWidth
-
-        bk.sizex = width
-        bk.sizey = btnHeight
-        bk.posy = height - btnHeight
-        bk.posx = 0
 
 
     def org_btn_press(instance):
@@ -43,24 +44,22 @@ class NavBarController(Widget):
 
 
 class TextEditor(Widget):
-    org_btn = ObjectProperty(None)
-    import_btn = ObjectProperty(None)
-    save_btn = ObjectProperty(None)
-    run_btn = ObjectProperty(None)
-    nav_bar_bk = ObjectProperty(None)
+    app_container = ObjectProperty(None)
+    nav_container = ObjectProperty(None)
+    text_container = ObjectProperty(None)
+    
     def setup(self):
         width = 500
         height = 500
         Window.size = (width, height)
-        NavBtns = [self.org_btn, self.import_btn, self.save_btn, self.run_btn]
-        NavBar = NavBarController()
-        NavBar.setup(width, height, NavBtns, self.nav_bar_bk)
+        Window.bind(on_resize=self.on_window_resize)
+        Config.set('graphics', 'resizable', '0')
+        navBar = NavBarController()
+        navBarBtnsContainer = navBar.setup(self.nav_container)
 
-
-
-
-
-
+    def on_window_resize(self, window, width, height):
+        print("width", width)
+        self.app_container.size = width, height
     #navBar = ObjectProperty(None)
 
 class MainApp(App):

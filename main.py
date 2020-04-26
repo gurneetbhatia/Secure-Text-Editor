a3a146df42a2a13c9552c856e0ecb634e8f4257c
bgurneet <sbgurneet@gmail.com>
2020-04-26 08:35:30 +0400

    working on creating org

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ import os
 
 import tkinter as tk
 
+cache = {'organisation': None, 'password': None}
+
 class NavBarController:
     fileDropdown = DropDown()
     orgDropdown = DropDown()
@@ -60,7 +62,7 @@ class NavBarController:
 
         #Organisaition Dropdown
         self.createOrg_btn = Button(text='Create Org', font_size=14, size_hint_y=None, height=30)
-        self.loadOrg_btn = Button(text='Load Org', font_size=14, size_hint_y=None, height=30)
+        self.loadOrg_btn = Button(text='Login to Org', font_size=14, size_hint_y=None, height=30)
         self.viewOrg_btn = Button(text='View Org', font_size=14, size_hint_y=None, height=30)
 
         self.createOrg_btn.bind(on_press=TextEditor.createOrg_btn_press)
@@ -136,6 +138,7 @@ class TextEditor(Widget):
         Window.size = (width, height)
         Window.bind(on_resize=self.on_window_resize)
         Config.set('graphics', 'resizable', '0')
+        Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
         Window.bind(on_keyboard=self.on_keyboard)
 
@@ -149,7 +152,6 @@ class TextEditor(Widget):
         self.text_container.add_widget(TextEditor.codeinput)
 
     def updateCodeInput(newstring):
-        print('hereeeeeeee')
         print(newstring)
         TextEditor.codeinput.text = newstring
 
@@ -181,6 +183,9 @@ class TextEditor(Widget):
     # ORG DROPDOWN EVENTS
     def createOrg_btn_press(instance):
         print("Create Org")
+        popup = CreateOrgPopup()
+        popup.setup()
+
     def loadOrg_btn_press(instance):
         print("Load Org")
     def viewOrg_btn_press(instance):
@@ -219,6 +224,71 @@ class TextEditor(Widget):
 
     def run_btn_press(instance):
         print('Run')
+
+class CreateOrgPopup(Widget):
+    org = ''
+    pas = ''
+    popup = None
+
+    def set_org(instance, value):
+        CreateOrgPopup.org = value
+
+    def get_org(self):
+        return CreateOrgPopup.org
+
+    def set_pas(instance, value):
+        CreateOrgPopup.pas = value
+
+    def get_pas(self):
+        return CreateOrgPopup.pas
+
+    def confirm(self):
+        pass
+
+    def setup(self):
+        layout = BoxLayout(orientation='vertical')
+        labelOrg = Label(text='Organisation')
+        inputOrg = TextInput(multiline=False)
+        labelPas = Label(text='Password')
+        inputPas = TextInput(multiline=False)
+        button = Button(text='Confirm')
+        layout.add_widget(labelOrg)
+        layout.add_widget(inputOrg)
+        layout.add_widget(labelPas)
+        layout.add_widget(inputPas)
+        layout.add_widget(button)
+        inputOrg.bind(text=CreateOrgPopup.set_org)
+        inputPas.bind(text=CreateOrgPopup.set_pas)
+        CreateOrgPopup.popup = Popup(title='Enter credentials for new Organisation',
+            content=layout, size_hint=(0.6, 0.6))
+        CreateOrgPopup.popup.open()
+        button.bind(on_press=self.confirm_clicked)
+        print('here1')
+
+    def confirm_clicked(self, x):
+        print('hereeeee')
+        CreateOrgPopup.popup.dismiss()
+        print('hereeee')
+        # validate the credentials
+        print(CreateOrgPopup.org, CreateOrgPopup.pas)
+        organisation = CreateOrgPopup.org
+        password = CreateOrgPopup.pas
+
+        fs = FileSystem()
+        # first check if an organisation with this name already exists
+        popup_msg = ""
+        err = False
+        try:
+            fs.getOrganisationKey(organisation, password)
+            popup_msg = "Organisation already exists!"
+            err = True
+        except FileNotFoundError:
+            # the organisation does not exist
+            fs.createOrganisation(organisation, password)
+            cache['organisation'] = organisation
+            cache['password'] = password
+
+
 
 class PopupInput(Widget):
     org = ''
@@ -284,6 +354,7 @@ class PopupInput(Widget):
         finally:
             layout = BoxLayout(orientation='vertical')
             popup_title = "Error" if err else "Confirmation"
+            label1 = Label(text=popup_title)
             label = Label(text=popup_msg)
             button = Button(text='Dismiss')
             layout.add_widget(label)
@@ -315,22 +386,21 @@ class LoadSaveDialog(FloatLayout):
     popup = None
 
     def set_org(instance, value):
-        PopupInput.org = value
+        LoadSaveDialog.org = value
 
     def get_org(self):
-        return PopupInput.org
+        return LoadSaveDialog.org
 
     def set_pas(instance, value):
-        PopupInput.pas = value
+        LoadSaveDialog.pas = value
 
     def get_pas(self):
-        return PopupInput.pas
+        return LoadSaveDialog.pas
 
     def dismiss_popup(self):
         self._popup.dismiss()
 
     def show_load(self):
-        print('here')
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
         self._popup = Popup(title="Load file", content=content,
                             size_hint=(0.9, 0.9))
@@ -349,30 +419,38 @@ class LoadSaveDialog(FloatLayout):
 
         print(path, filename)
         # ask the user for organisation name and password
-        layout = BoxLayout(orientation='vertical')
-        labelOrg = Label(text='Organisation')
-        inputOrg = TextInput(multiline=False)
-        labelPas = Label(text='Password')
-        inputPas = TextInput(multiline=False)
-        button = Button(text='Confirm')
-        layout.add_widget(labelOrg)
-        layout.add_widget(inputOrg)
-        layout.add_widget(labelPas)
-        layout.add_widget(inputPas)
-        layout.add_widget(button)
-        inputOrg.bind(text=PopupInput.set_org)
-        inputPas.bind(text=PopupInput.set_pas)
-        LoadSaveDialog.popup = Popup(title='Enter credentials',
-            content=layout, size_hint=(None, None), size=(400, 250))
-        LoadSaveDialog.popup.open()
-        button.bind(on_press=self.confirm_clicked)
-        '''if ext != 'enc':
-            # the file needs to be encrypted
-            '''
+        if (cache['organisation'] != None):
+            # the user is logged in to an org
+            if ext != 'enc':
+                # the file needs to be encrypted first
+                print('here3')
+                fs.createFile(filename[0], organisation, password)
+                print(TextEditor.codeinput.text)
+                TextEditor.updateCodeInput(LoadSaveDialog.string)
+                print(TextEditor.codeinput.text)
+                print(LoadSaveDialog.string)
+            else:
+                print(fs.readFile(filename, organisation, password))
+                TextEditor.codeinput.text = fs.readFile(filename, organisation, password)
+        else:
+            # the user needs to be prompted to login first
+            popup_msg = "Please login to an organisation first!"
+            layout = BoxLayout(orientation='vertical')
+            popup_title = "Error"
+            label1 = Label(text=popup_title)
+            label = Label(text=popup_msg)
+            button = Button(text='Dismiss')
+            layout.add_widget(label1)
+            layout.add_widget(label)
+            layout.add_widget(button)
+            popup = Popup(title=popup_title,
+                content=layout, size_hint=(0.6, 0.6))
+            popup.open()
+            button.bind(on_press=popup.dismiss)
 
         self.dismiss_popup()
 
-    def confirm_clicked(self, x):
+    '''def confirm_clicked(self, x):
         LoadSaveDialog.popup.dismiss()
         filename = LoadSaveDialog.file_to_load
         # validate the credentials
@@ -423,7 +501,7 @@ class LoadSaveDialog(FloatLayout):
                 content=layout, size_hint=(None, None), size=(400, 250))
             popup.open()
             button.bind(on_press=popup.dismiss)
-            print("Done")
+            print("Done")'''
 
     def save(self, path, filename):
         with open(os.path.join(path, filename), 'w') as stream:

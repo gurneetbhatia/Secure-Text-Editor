724a67b0033d4d55313cdcaa5046e51ab33da3bc
bgurneet <sbgurneet@gmail.com>
2020-04-26 12:42:15 +0400

    made key size double

--- encrypt.py
+++ encrypt.py
@@ -11,7 +11,7 @@ class Encrypt:
 
     def getNewKey(self):
         return rsa.generate_private_key(public_exponent=65537,
-        key_size=2048,
+        key_size=4096,
         backend=default_backend())
 
     def getPublicKey(self):
